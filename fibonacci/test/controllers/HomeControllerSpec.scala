5934796f0ee757c7aa7007eb283e37415b7b473a
anako18 <nuka97@yandex.ru>
2018-01-21 23:03:26 +0300

    some tests added

--- fibonacci/test/controllers/HomeControllerSpec.scala
+++ fibonacci/test/controllers/HomeControllerSpec.scala
@@ -4,24 +4,19 @@ import org.scalatestplus.play._
 import org.scalatestplus.play.guice._
 import play.api.test._
 import play.api.test.Helpers._
+import computation.Fibonacci
 
-/**
- * Add your spec here.
- * You can mock out a whole application including requests, plugins etc.
- *
- * For more information, see https://www.playframework.com/documentation/latest/ScalaTestingWithScalaTest
- */
 class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting {
 
   "HomeController GET" should {
 
     "render the index page from a new instance of controller" in {
-      val controller = new HomeController(stubControllerComponents())
+      val fibonacci = new Fibonacci()
+      val controller = new HomeController(stubControllerComponents(),fibonacci)
       val home = controller.index().apply(FakeRequest(GET, "/"))
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
     }
 
     "render the index page from the application" in {
@@ -30,7 +25,6 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
     }
 
     "render the index page from the router" in {
@@ -39,7 +33,29 @@ class HomeControllerSpec extends PlaySpec with GuiceOneAppPerTest with Injecting
 
       status(home) mustBe OK
       contentType(home) mustBe Some("text/html")
-      contentAsString(home) must include ("Welcome to Play")
+    }
+
+    "cashing" in {
+       val fib = new Fibonacci()
+       fib.cashed_elems.cnt mustBe 2
+       fib.cashed_elems.elems(0) mustBe 1
+       fib.fib_compute(10)
+       fib.cashed_elems.cnt mustBe 10
+       fib.fib_compute(3)
+       fib.cashed_elems.cnt mustBe 10
+       fib.cashed_elems.elems(9) mustBe 55
+       fib.cashed_elems.elems(8) mustBe 34	
+     }
+	
+     "computing" in {
+       val fib = new Fibonacci()
+       fib.fib_compute(10)
+       fib.cashed_elems.elems.count(x => x <= 0) mustBe 0
+       fib.take_n_elems(5)(4) mustBe 5
+       fib.fib_compute(10)
+       fib.take_n_elems(10).last mustBe 55
+       fib.fib_compute(30)
+       fib.cashed_elems.elems.last mustBe 832040
     }
   }
 }
