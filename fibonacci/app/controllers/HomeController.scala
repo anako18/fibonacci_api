59f2d7c5ef52804e072e352259ba71f43eea2558
anako18 <nuka97@yandex.ru>
2018-01-22 00:45:32 +0300

    Throwing negative count exception added

--- fibonacci/app/controllers/HomeController.scala
+++ fibonacci/app/controllers/HomeController.scala
@@ -11,13 +11,14 @@ import computation.Fibonacci
 class HomeController @Inject()(cc: ControllerComponents, fib:Fibonacci) extends AbstractController(cc) {
 																																																			
   def fib_sequence(count: Int) = Action {
+    if (count < 0) { throw new IllegalArgumentException("Count must be positive.") }
     fib.fib_compute(count)
     val printed_sequence = fib.take_n_elems(count).mkString("", ", ", ".")
   Ok(views.html.index("First " + count + " elements: " + printed_sequence))
   }
 
   def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index("Pass the count of fibonacci numbers as a parameter: /fibonacci?count=YOUR_NUMBER"))
+    Ok(views.html.index("Pass the positive int count of fibonacci numbers as a parameter: /fibonacci?count=YOUR_NUMBER"))
   }
 }
 
