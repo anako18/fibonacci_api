f936310288bdfcb946202b6c195ac6612a9b552c
anako18 <nuka97@yandex.ru>
2018-01-21 09:24:58 +0300

    Simple print of n-th element

--- fibonacci/app/controllers/HomeController.scala
+++ fibonacci/app/controllers/HomeController.scala
@@ -4,20 +4,28 @@ import javax.inject._
 import play.api._
 import play.api.mvc._
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
+
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
+
+  def fib_sequence(count: Int) = Action {
+    var res = fib(count) 
+    Ok( count + "th element is " + res )
+  }
+
+
+  def fib(n: Int) : Int = {
+  var result = 0
+  if (n>1) {result = fib(n-1) + fib(n-2)
+  } 
+  else {
+  result = n
+  }
+
+  return result			
+  }
+
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
