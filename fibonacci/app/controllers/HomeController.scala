f2f3947cbc7e49847e06f6c6823229194902f059
anako18 <nuka97@yandex.ru>
2018-01-21 15:09:41 +0300

    Print the sequence with cashing implemented

--- fibonacci/app/controllers/HomeController.scala
+++ fibonacci/app/controllers/HomeController.scala
@@ -3,30 +3,52 @@ package controllers
 import javax.inject._
 import play.api._
 import play.api.mvc._
+import scala.collection.mutable.ListBuffer
 
+object cashed_elems{
+  var elems = ListBuffer[Long](1,1)
+  def cnt = elems.length //count of cached elems
+}
 
 @Singleton
 class HomeController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-
-
+																																																			
   def fib_sequence(count: Int) = Action {
-    var res = fib(count) 
-    Ok( count + "th element is " + res )
+    var res = if (count > cashed_elems.cnt) fib(count, cashed_elems.cnt) else 0
+    var s = print_n(count)
+    Ok("First " + count + " elements: " + s)
   }
 
+  def fib(n: Int, from : Int) : Long = {
+    var a : Long = cashed_elems.elems(from-2) 
+    var b : Long = cashed_elems.elems(from-1)
+    var i = from-1	  
+ 
+    while( i < n-1 ) {
+      var c : Long = a + b
 
-  def fib(n: Int) : Int = {
-  var result = 0
-  if (n>1) {result = fib(n-1) + fib(n-2)
-  } 
-  else {
-  result = n
+      cashed_elems.elems+=c
+
+      a = b
+      b = c
+      i = i + 1
+    } 
+    a = b
+    a
   }
 
-  return result			
-  }
+  def print_n(n: Int) : String = {
+    var s = ""
+    for (i <- 0 to n-1) {
+      s = s + cashed_elems.elems(i) + " "
+    }
+    s
+  }  
 
   def index() = Action { implicit request: Request[AnyContent] =>
     Ok(views.html.index())
   }
 }
+
+
+
